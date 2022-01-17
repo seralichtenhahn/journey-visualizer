f72ece363b932aead1931b79800ba20cfc170c76
Serafin Lichtenhahn <s.lichtenhahn0420201@arts.ac.uk>
2022-01-17 16:40:17 +0000

    use env variables in config file

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,18 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig, loadEnv } from 'vite'
+
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": "/src",
+export default ({ mode }) => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) }
+
+  return defineConfig({
+    plugins: [react()],
+    base: process.env.VITE_BASE_PATH,
+    resolve: {
+      alias: {
+        '@': '/src',
+      },
     },
-  },
-});
+  })
+}
