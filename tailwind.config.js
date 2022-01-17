7a6f78743802eb482b1f9e6b3d9f8d05e7026c3c
Serafin Lichtenhahn <s.lichtenhahn0420201@arts.ac.uk>
2022-01-17 16:39:38 +0000

    update tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,39 @@
 module.exports = {
-  mode: "jit",
-  purge: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-  darkMode: false, // or 'media' or 'class'
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        serif: ['Arvo', 'serif'],
+        sans: ['DM Sans', 'sans-serif'],
+      },
+      colors: {
+        primary: {
+          default: '#98EBD2',
+          200: '#D6F7ED',
+          300: '#C1F3E4',
+          400: '#ADEFDB',
+          500: '#98EBD2',
+          600: '#89D4BD',
+          700: '#7ABCA8',
+        },
+        gray: {
+          default: '#9D9D9F',
+          100: '#FDFCFC',
+          200: '#EBEAEA',
+          300: '#D1D1D2',
+          400: '#BABBBC',
+          500: '#9D9D9F',
+          600: '#717171',
+          700: '#48494C',
+          800: '#1a1b1f',
+          900: '#000000',
+        },
+        icons: '#696969',
+      },
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-};
+}
